2f0422c5df2aba1a9e844d4cc35639e853b25934
ZoeYiDing <flarejennie@gmail.com>
2025-07-19 01:38:35 +1000

    Add files via upload

---
+++ kuzu.py
@@ -0,0 +1,40 @@
+"""
+   kuzu.py
+"""
+
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class NetLin(nn.Module):
+    # linear function followed by log_softmax
+    def __init__(self):
+        super(NetLin, self).__init__()
+        # INSERT CODE HERE
+
+    def forward(self, x):
+        return 0 # CHANGE CODE HERE
+
+class NetFull(nn.Module):
+    # two fully connected tanh layers followed by log softmax
+    def __init__(self):
+        super(NetFull, self).__init__()
+        # INSERT CODE HERE
+
+    def forward(self, x):
+        return 0 # CHANGE CODE HERE
+
+class NetConv(nn.Module):
+    # two convolutional layers and one fully connected layer,
+    # all using relu, followed by log_softmax
+    def __init__(self):
+        super(NetConv, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 100)
+        self.fc2 = nn.Linear(100, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = torch.tanh(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
